726055845b20610681f1e6928b1e6d4b8cefcf5c
Mesekir19 <mesekirsalgech@gmail.com>
2023-12-30 19:57:47 +0300

    Nike Shoe Store

---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,38 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images"
+import Button from "../components/Button";
+const SpecialOffer = () => {
+  return (
+    <section className=" flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container ">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="offer"
+          width={773}
+          height={687}
+          className=" object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className=" text-coral-red inline-block mt-3">Special</span>
+          <span className=" text-coral-red inline-block mt-3">Offer</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover style Nike arrivals, Quality comfort, and innovation for your
+          active life.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Discover style Nike arrivals, Quality comfort, and innovation for your
+          active life.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop Now" iconURL={arrowRight} />
+          <Button label="Learn More" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray"/>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SpecialOffer
